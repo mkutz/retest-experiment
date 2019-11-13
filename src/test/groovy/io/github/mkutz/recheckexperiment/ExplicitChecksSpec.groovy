b76b1c1cc9514430febb55e057f93720068d7f08
mkutz <michael.kutz@rewe-digital.com>
2019-11-13 10:24:57 +0100

    Inital state for Restest

--- src/test/groovy/io/github/mkutz/recheckexperiment/ExplicitChecksSpec.groovy
+++ src/test/groovy/io/github/mkutz/recheckexperiment/ExplicitChecksSpec.groovy
@@ -1,11 +1,27 @@
 package io.github.mkutz.recheckexperiment
 
+import de.retest.recheck.Recheck
+import de.retest.recheck.RecheckImpl
 import geb.spock.GebSpec
 
 class ExplicitChecksSpec extends GebSpec {
 
+    Recheck re = new RecheckImpl()
+
+    def setup() {
+        re.startTest("ExplicitChecks")
+    }
+
     def "should work"() {
-        expect:
-        browser.go "https://google.com"
+        when:
+        go "https://google.com"
+
+        then:
+        re.check(browser.driver, "open")
+        re.capTest()
+    }
+
+    def cleanup() {
+        re.cap()
     }
 }
